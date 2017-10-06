78a72a7eb3bbd57afe9f88d49b0a77ecc1a095df
David P. Bauer <dbauer1@udayton.edu>
2017-10-06 16:02:40 -0400

    Review mode working WIP

--- playcard.php
+++ playcard.php
@@ -8,6 +8,22 @@ $LAUNCH = LTIX::requireData();
 
 $p = $CFG->dbprefix;
 
+// Comparator for card number
+function compareCardNum2($a, $b) {
+
+    if ($a["CardNum2"] == $b["CardNum2"]) {
+        return 0;
+    }
+    return ($a["CardNum2"] < $b["CardNum2"]) ? -1 : 1;
+}
+function compareCardNum($a, $b) {
+
+    if ($a["CardNum"] == $b["CardNum"]) {
+        return 0;
+    }
+    return ($a["CardNum"] < $b["CardNum"]) ? -1 : 1;
+}
+
 $OUTPUT->header();
 
 include("tool-header.html");
@@ -47,37 +63,122 @@ if ( $USER->instructor ) {
     }
 }
 
+if(isset($_GET["ReviewMode"])){
+    $isReviewMode = $_GET["ReviewMode"];
+} else {
+    $isReviewMode = 0;
+}
+
 $setId = $_GET["SetID"];
 $_SESSION["SetID"] = $setId;
 
 $cardsInSet = $PDOX->allRowsDie("SELECT * FROM {$p}flashcards where SetID=".$setId.";");
 
-$set = $PDOX->rowDie("select * from {$p}flashcards_set where SetID=".$setId.";");
+if($CardNum == 0){
+    $theCard = $PDOX->rowDie("SELECT * FROM {$p}flashcards where SetID=".$setId." AND CardNum2=".$CardNum2.";");
+} else {
+    $theCard = $PDOX->rowDie("SELECT * FROM {$p}flashcards where SetID=".$setId." AND CardNum=".$CardNum.";");
+}
+
+$cardKnown = $PDOX->rowDie("SELECT * FROM {$p}flashcards_review WHERE UserId = '".$USER->id."' AND SetId ='".$setId."' AND CardId = '".$theCard["CardID"]."';");
+
+if($isReviewMode == 1) {
+
+    if($cardKnown && $CardNum == 0) {
+        header( 'Location: '.addSession('playcard.php?CardNum=0&CardNum2='.++$CardNum2.'&Flag=A&SetID='.$setId.'&Shortcut='.$shortCut.'&ReviewMode='.$isReviewMode) ) ;
+    } else if ($cardKnown && $CardNum2 == 0) {
+        header( 'Location: '.addSession('playcard.php?CardNum='.++$CardNum.'&CardNum2=0&Flag=A&SetID='.$setId.'&Shortcut='.$shortCut.'&ReviewMode='.$isReviewMode) ) ;
+    }
+
+    $knownCards = $PDOX->allRowsDie("SELECT CardID FROM {$p}flashcards_review WHERE UserId = '".$USER->id."' AND SetId = '".$setId."';");
+
+    $knownCardIds = array();
+
+    $x = 0;
+    foreach($knownCards as $knownCard) {
+        $knownCardIds[$x] = $knownCard["CardID"];
+        $x++;
+    }
+
+    foreach($cardsInSet as $key => $card) {
+        if (in_array($card["CardID"], $knownCardIds)){
+            unset($cardsInSet[$key]);
+        }
+    }
+}
 
 $Total = count($cardsInSet);
 
+$set = $PDOX->rowDie("select * from {$p}flashcards_set where SetID=".$setId.";");
+
 if ($CardNum == 0) {
     // We are in "shuffle" mode
+
+    usort($cardsInSet, "compareCardNum2");
+
     $Next = 0;
     $Prev = 0;
-    $Next2 = $CardNum2 + 1;
-    $Prev2 = $CardNum2 - 1;
-    $percentComplete = $CardNum2 / $Total * 100;
-    $theCard = $PDOX->rowDie("SELECT * FROM {$p}flashcards where SetID=".$setId." AND CardNum2=".$CardNum2.";");
-    $onCard = $CardNum2;
+
+    reset($cardsInSet);
+    $position = 1;
+    $Prev2 = 0;
+    $Next2 = 0;
+    for ($x = 1; $x <= $Total; $x++) {
+        if (current($cardsInSet)["CardNum2"] == $CardNum2) {
+            $position = $x;
+
+            if ($x > 1) {
+                $Prev2 = prev($cardsInSet)["CardNum2"];
+                // Set back to current
+                next($cardsInSet);
+            }
+
+            if ($x < $Total) {
+                $Next2 = next($cardsInSet)["CardNum2"];
+            }
+
+            break;
+        } else {
+            next($cardsInSet);
+        }
+    }
+
+    $percentComplete = $position / $Total * 100;
 } else {
-    $Next = $CardNum + 1;
-    $Prev = $CardNum - 1;
+
+    usort($cardsInSet, "compareCardNum");
+
+    reset($cardsInSet);
+    $position = 1;
+    $Prev = 0;
+    $Next = 0;
+    for ($x = 1; $x <= $Total; $x++) {
+        if (current($cardsInSet)["CardNum"] == $CardNum) {
+            $position = $x;
+
+            if ($x > 1) {
+                $Prev = prev($cardsInSet)["CardNum"];
+                // Set back to current
+                next($cardsInSet);
+            }
+
+            if ($x < $Total) {
+                $Next = next($cardsInSet)["CardNum"];
+            }
+
+            break;
+        } else {
+            next($cardsInSet);
+        }
+    }
+
     $Next2 = 0;
     $Prev2 = 0;
-    $percentComplete = $CardNum / $Total * 100;
-    $theCard = $PDOX->rowDie("SELECT * FROM {$p}flashcards where SetID=".$setId." AND CardNum=".$CardNum.";");
-    $onCard = $CardNum;
+
+    $percentComplete = $position / $Total * 100;
 }
 
 $_SESSION["CardId"] = $theCard["CardID"];
-
-$cardKnown = $PDOX->rowDie("SELECT * FROM {$p}flashcards_review WHERE UserId = '".$USER->id."' AND SetId ='".$setId."' AND CardId = '".$theCard["CardID"]."';");
 
     if ($shortCut == 0) {
         echo('
@@ -92,21 +193,21 @@ $cardKnown = $PDOX->rowDie("SELECT * FROM {$p}flashcards_review WHERE UserId = '
         <div class="row cardRow">
             <div class="col-sm-3 play-menu">
                 <h3>'.$set["CardSetName"].'</h3>
-                <span>Progress <strong>'.$onCard.'/'.$Total.'</strong></span>
+                <span>Progress <strong>'.$position.'/'.$Total.'</strong></span>
                 <div class="progress">
                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="'.$percentComplete.'" aria-valuemin="0" aria-valuemax="100" style="width:'.$percentComplete.'%">
                         <span class="sr-only">'.$percentComplete.'% Complete</span>
                     </div>
                 </div>
                 <p>
-                    <a class="btn btn-primary" href="shuffle.php?SetID='.$set["SetID"].'&Shortcut='.$shortCut.'"><span class="fa fa-random"></span> Shuffle Cards</a>                
+                    <a class="btn btn-primary" href="shuffle.php?SetID='.$set["SetID"].'&Shortcut='.$shortCut.'&ReviewMode='.$isReviewMode.'"><span class="fa fa-random"></span> Shuffle Cards</a>                
                 </p>
                 <p>');
 
-                if(isset($_GET["ReviewMode"]) && $_GET["ReviewMode"] == 1) {
-                    echo('<a class="btn btn-success" href="playcard.php?SetID='.$set["SetID"].'&CardNum=1&CardNum2=0&Flag=A&ReviewMode=0"><span class="fa fa-check-square-o"></span> Review Mode</a>');
+                if($isReviewMode == 1) {
+                    echo('<a class="btn btn-success" href="playcard.php?SetID='.$set["SetID"].'&CardNum=1&CardNum2=0&Flag=A&ReviewMode=0&Shortcut='.$shortCut.'"><span class="fa fa-check-square-o"></span> Review Mode</a>');
                 } else {
-                    echo('<a class="btn btn-default" href="playcard.php?SetID='.$set["SetID"].'&CardNum=1&CardNum2=0&Flag=A&ReviewMode=1"><span class="fa fa-square-o"></span> Review Mode</a>');
+                    echo('<a class="btn btn-default" href="playcard.php?SetID='.$set["SetID"].'&CardNum=1&CardNum2=0&Flag=A&ReviewMode=1&Shortcut='.$shortCut.'"><span class="fa fa-square-o"></span> Review Mode</a>');
                 }
 
                 echo('</p>
@@ -167,13 +268,13 @@ $cardKnown = $PDOX->rowDie("SELECT * FROM {$p}flashcards_review WHERE UserId = '
                 echo('</span> I know this card</a>
                         
                 <div class="prev-next text-center">
-                    <a href="playcard.php?SetID='.$setId.'&CardNum='.$Prev.'&CardNum2='.$Prev2.'&Flag=A&Shortcut='.$shortCut.'" ');if($Prev == 0 && $Prev2 == 0){echo('class="disabled"');} echo('>
+                    <a href="playcard.php?SetID='.$setId.'&CardNum='.$Prev.'&CardNum2='.$Prev2.'&Flag=A&Shortcut='.$shortCut.'&ReviewMode='.$isReviewMode.'" ');if($Prev == 0 && $Prev2 == 0){echo('class="disabled"');} echo('>
                         <span class="fa fa-3x fa-chevron-circle-left"></span>
                     </a>
                     <a href="javascript:void(0);" id="flip-link">
                         <span class="fa fa-3x fa-undo"></span>
                     </a>
-                    <a id="next-link" href="playcard.php?SetID='.$setId.'&CardNum='.$Next.'&CardNum2='.$Next2.'&Flag=A&Shortcut='.$shortCut.'" ');if($Next > $Total || $Next2 > $Total){echo('class="disabled"');} echo('>
+                    <a id="next-link" href="playcard.php?SetID='.$setId.'&CardNum='.$Next.'&CardNum2='.$Next2.'&Flag=A&Shortcut='.$shortCut.'&ReviewMode='.$isReviewMode.'" ');if(!$Next && !$Next2){echo('class="disabled"');} echo('>
                         <span class="fa fa-3x fa-chevron-circle-right"></span>
                     </a>
                 </div>
--- shuffle.php
+++ shuffle.php
@@ -21,6 +21,12 @@ if(isset($_GET["Shortcut"])) {
     $shortCut = 0;
 }
 
+if(isset($_GET["ReviewMode"])) {
+    $reviewMode = $_GET["ReviewMode"];
+} else {
+    $reviewMode = 0;
+}
+
 $NewCardNum = array(); // temp for CardNum2
 
 $allCards = $PDOX->allRowsDie("SELECT CardNum, CardID FROM {$p}flashcards where SetID=".$SetID.";");
@@ -44,4 +50,4 @@ for ($x = 1; $x <= $TotalCards; $x++) {
     $PDOX->queryDie("update {$p}flashcards set CardNum2=".$CardNum2." where CardNum=".$CardNum." AND SetID=".$SetID);
 }
 
-header( 'Location: '.addSession('playcard.php?CardNum=0&CardNum2=1&Flag=A&SetID='.$SetID.'&Shortcut='.$shortCut) ) ;
+header( 'Location: '.addSession('playcard.php?CardNum=0&CardNum2=1&Flag=A&SetID='.$SetID.'&Shortcut='.$shortCut.'&ReviewMode='.$reviewMode) ) ;
