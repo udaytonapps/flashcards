b87fffed05dd26cdb85ea5c299fa10b23ac085ed
Andrew Niekamp <aniekamp1@udayton.edu>
2022-05-24 15:44:56 -0400

    TSUGI-308 Add images and update description

--- register.php
+++ register.php
@@ -4,6 +4,24 @@ $REGISTER_LTI2 = array(
     "name" => "Flashcards", // Name of the tool
     "FontAwesome" => "fa-clone", // Icon for the tool
     "short_name" => "Flashcards",
-    "description" => "Create flashcard sets that include text, images, MP3s, or videos that your students can use to study important material in your course sites. They are fun and work great on a browser or mobile device.", // Tool description
-    "messages" => array("launch", "launch_grade")
+    "description" => "Create flashcard sets that students can use to study. Text, images, audio, or video can be added to cards.", // Tool description
+    "messages" => array("launch", "launch_grade"),
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+    "video" => "https://udayton.warpwire.com/w/FfsEAA/",
+    "source_url" => "https://github.com/udaytonapps/flashcards",
+    "screen_shots" => array(
+        "images/Using-Flash-Card.png",
+        "images/New-Card-Set.png",
+        "images/Card-Sets.png",
+        "images/Setting-up-Card.png",
+        "images/Card.png",
+        "images/Edit-Flashcard.png",
+        "images/Flashcard-Isidore.png",
+    )
 );
